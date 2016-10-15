26adb34443b9a922486d53751e2fa023301fd0e2
Khaleeq Ahmad <kahmad28@gmail.com>
2016-10-15 17:46:22 +0100

    Move repo to contents of User folder only

--- Ruby Debugger/execute.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-exec ruby '-C/home/shuky/develoment/shopli' '-rbundler/setup' '-r/home/shuky/.config/sublime-text-3/Packages/Ruby Debugger/sublime_debug_require.rb' '/home/shuky/develoment/shopli/script/rails' s
